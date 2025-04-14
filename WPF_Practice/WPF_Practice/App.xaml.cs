3356554133fc7aae47f6a9da5c63cc78f9f4e154
MilenaRPotes <110207483+MilenaRPotes@users.noreply.github.com>
2025-04-14 17:45:24 -0500

    first configuration of vews,diccionary resource and usermodel

--- WPF_Practice/WPF_Practice/App.xaml.cs
+++ WPF_Practice/WPF_Practice/App.xaml.cs
@@ -13,5 +13,12 @@ namespace WPF_Practice
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ WPF_Practice/WPF_Practice/Models/UserModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Practice.Models
+{
+    public class UserModel
+    {
+        private string _name;
+        private string _lastName;
+        private string _email;
+        private string _password;
+
+        public string Name 
+        { 
+             get => _name; 
+             set 
+                { 
+                    if (_name != value)  _name = value; 
+                } 
+        
+        }
+        public string LastName 
+        { 
+            get => _lastName; 
+            set 
+            { 
+                if(_lastName != value) _lastName = value;
+            }
+        
+        }
+        public string Email 
+        {
+            get => _email;
+            set 
+            {
+                if (_email != value) _email = value; 
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (_password != value) _password = value;
+            }
+        }  
+    }
+
+}
